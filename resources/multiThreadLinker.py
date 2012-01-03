747fefc1af0d7fe834b4eed7a0ba70e89c3e690c
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2012-01-03 21:41:44 +0100

    first commit

---
+++ resources/multiThreadLinker.py
@@ -0,0 +1,89 @@
+import threading, time, urllib, sys, signal, math
+
+# HANDLE STARTUP
+link2curl = ''
+threads2create = 0
+maxCounter = -1
+if len(sys.argv) != 3:
+	if len(sys.argv) == 4:
+		maxCounter = int(sys.argv[3])
+	else:
+		print "Usage: %s <url to site> <number of threads> [number of clicks]" % (sys.argv[0])
+		sys.exit()
+link2curl = str(sys.argv[1])
+threads2create = int(sys.argv[2])
+ID = 1
+counter = 0
+running = True
+times = []
+
+# NEEDED FUNCTIONS
+def genMDEV():
+	avg = sum(times)/len(times)
+	res = []
+	for i in times:
+		res.append(pow(i - avg, 2))
+	mdev = math.sqrt(sum(res)/len(res))
+	return mdev
+def showStats():
+	print "--- Curled \"%s\" with %i threads statistics ---" % (link2curl, threads2create)
+	print "%i clicks generated, time %ims" % (
+		counter,
+		(endT - startT) * 1000
+	)
+	if maxCounter != -1 and maxCounter <= len(t):
+		print "Just flooding your resource..."
+		sys.exit()
+	print "rtt min/avg/max/mdev = %.3f/%.3f/%.3f/%.3f ms" % (
+		min(times),
+		sum(times)/len(times),
+		max(times),
+		genMDEV()
+	)
+	sys.exit()
+
+# NEEDED CLASSES
+class curli(threading.Thread):
+	def run(self):
+		global ID, counter, running, times
+		myID = ID
+		myCounter = 0
+		ID += 1
+		while running:
+			myCounter += 1
+			counter += 1
+			print "[%i] : %i" % (myID, myCounter)
+			first = time.time()
+			urllib.urlopen(link2curl)
+			second = time.time()
+			times.append((second - first) * 1000)
+
+#signal.signal(signal.SIGINT, showStats)
+#signal.signal(signal.SIGTERM, showStats)
+#signal.signal(signal.SIGQUIT, showStats)
+
+# START THREADS
+t = []
+startT = time.time()
+for i in range(threads2create):
+	t.append(curli())
+	if maxCounter != -1 and i >= maxCounter:
+		break
+for i in t:
+	i.start()
+
+# HANDLE DEAD THREADS
+while running:
+	allDead = True
+	for i in t:
+		if i.isAlive():
+			allDead = False
+	if allDead:
+		showStats()
+		sys.exit()
+
+	if maxCounter != -1 and counter >= maxCounter:
+		endT = time.time()
+		running = False
+
+showStats()
---
+++ timeTester.py
@@ -0,0 +1,100 @@
+import popen2, sys, url2load
+
+if len(sys.argv) != 3:
+	print "Usage: <how many clicks> <how many threads at maximum>"
+	sys.exit()
+
+COUNTER = int(sys.argv[1])
+THREADMAX = int(sys.argv[2])
+
+URL = url2load.URL
+
+def execCurl(threadNum):
+	'''
+	Returns duration of curling for given thread number
+	'''
+	command = 'resources/elapsedTime.sh "python2.7 resources/multiThreadLinker.py %s %i %i"' % (URL, threadNum, COUNTER)
+	r, w, e = popen2.popen3(command)
+	output = r.readlines()
+	r.close()
+	e.close()
+	w.close()
+	return output
+
+print "Executing command to gain %i clicks with up to %i threads." % (COUNTER, THREADMAX)
+
+fd = open('plainData/data_%i-%i' % (COUNTER, THREADMAX), 'a+')
+
+r = {}
+for i in range(1, THREADMAX + 1):
+	print ">> Running with %i thread(s)..." % i
+	# r[number of threads] = duration of requests
+	r[i] = execCurl(i)
+
+print "Finished loading information."
+print "Now write it to file."
+for i in r.keys():
+	key = int(i)
+	val = int(r[i][0].strip())
+	print ">> Write: \"%i   %i\"..." % (key, val)
+	print >> fd, "%i   %i" % (key, val)
+
+fd.close()
+
+t = []
+for i in r.values():
+	t.append(i)
+m = max(t)[0]
+MAXTIME = round(int(m) + 5 * pow(10, len(str(m))-2) , -(len(str(m))-1))
+
+print "Now drawing graph."
+print ">> Generating corresponding plotter..."
+sample = '''# auto generated plotter
+set autoscale
+unset log
+unset label
+set term png
+set output 'graphs/%i-%i.png'
+set xtic auto
+set ytic auto
+set title "Thread-Amount and Time Relation (%i clicks)"
+set xlabel "Amount of Threads"
+set ylabel "Elapsed Time"
+set xr [0:%i]
+set yr [0:%i]
+plot 'plainData/data_%i-%i' using 1:2 title 'Developement of Time' with linespoints
+# hope you enjoy''' % (COUNTER, THREADMAX, COUNTER, THREADMAX, MAXTIME, COUNTER, THREADMAX)
+fd = open('plotter/makePlot_%i-%i' % (COUNTER, THREADMAX), 'a+')
+print >> fd, sample
+fd.close()
+print ">> Drawing normal graph..."
+r, w, e = popen2.popen3('gnuplot plotter/makePlot_%i-%i' % (COUNTER, THREADMAX))
+r.close()
+e.close()
+w.close()
+
+print ">> Generating interpolated plotter..."
+sample = '''# auto generated plotter
+set autoscale
+unset log
+unset label
+set term png
+set output 'graphs/%i-%i_smooth.png'
+set xtic auto
+set ytic auto
+set title "Thread-Amount and Time Relation (%i clicks)"
+set xlabel "Amount of Threads"
+set ylabel "Elapsed Time"
+set xr [0:%i]
+set yr [0:%i]
+plot 'plainData/data_%i-%i' using 1:2 smooth csplines title 'Developement of Time' with linespoints
+# hope you enjoy''' % (COUNTER, THREADMAX, COUNTER, THREADMAX, MAXTIME, COUNTER, THREADMAX)
+fd = open('plotter/makePlot_%i-%i_smooth' % (COUNTER, THREADMAX), 'a+')
+print >> fd, sample
+fd.close()
+print ">> Drawing interpolated graph..."
+r, w, e = popen2.popen3('gnuplot plotter/makePlot_%i-%i_smooth' % (COUNTER, THREADMAX))
+r.close()
+e.close()
+w.close()
+
